136ba7fcef2eec5c6054e85505e0cb717801546e
Ahmed Abdelaal <ahmed.moustafa@code95.info>
2019-04-04 10:40:13 +0200

    merge file

--- App/middleware/auth.js
+++ App/middleware/auth.js
@@ -5,9 +5,8 @@ const Jwt = require('./../JwtHelper/Jwt');
  */
 function auth(req, res, next) {
     var token = req.headers['access-token'];
-    if(!token){
-        return res.status(419).json({error:"No token provided"})
-    }
+    if(!token) return res.status(419).json({error:"No token provided"})
+
     const decode=Jwt.verfiyToken(token,process.env.JWT_TOKEN_SECRET)
     if(!decode) return res.status(419).json({error:'invalid token'})
     req.id = decode._id
